aedfff694f373e70bdb1e04cbb941cb2eddc5a02
Imran Rather <emmeiwhite@gmail.com>
2021-11-23 23:49:04 +0530

    simple color flipper done!

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const body = document.body;
+const button = document.getElementById("btn");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", (e) => {
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+});
